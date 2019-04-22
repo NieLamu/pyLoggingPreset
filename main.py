a2352dc7ca6eebddaf5c525c9049834962c8c206
NieLamu <mxmxlty@gmail.com>
2019-04-22 13:10:57 +0800

    add preset

---
+++ main.py
@@ -0,0 +1,13 @@
+import logging
+import logging.config
+import os
+
+
+def get_logger(config_name='log', name=__name__):
+    conf_log = os.path.join(os.path.dirname(__file__), "%s.ini" % config_name)
+    print(name, conf_log)
+    logging.config.fileConfig(conf_log)
+    return logging.getLogger(name)
+
+
+# get_logger(__name__)
